fa01d635d1f9e97544542049afb543a7369c3f5c
cphung1 <cphung97@gmail.com>
2020-02-13 03:17:27 +0000

    inital commit

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const args = process.argv;
+const result = args.slice(2);
+const fs = require('fs');
+let address = result[0]
+let path = result[1]
+
+request(address, (error, response, body) => {
+  // console.log('body:', body);
+  // Print the HTML for the Google homepage.
+  // console.log(body);
+  if (error) {
+    console.log(error);
+    process.exit();
+  }
+  fs.writeFile(path, body, error => {
+    if (error) {
+      throw error;
+    } 
+    fs.stat(path, (error, stat) => {
+      if (error) {
+        throw error;
+      }
+      console.log(stat.size);
+    })
+
+  })
+
+});
